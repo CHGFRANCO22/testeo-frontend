1feea4123a6ba00ddcb8b0bf0b9125482c892bdc
CHGFRANCO22 <francopudge22@gmail.com>
2025-06-20 18:29:43 -0300

    Update turnos.js

--- backend/routes/turnos.js
+++ backend/routes/turnos.js
@@ -2,12 +2,15 @@ const express = require('express');
 const router = express.Router();
 
 const { crearTurno, obtenerTurnosPorPaciente } = require('../controllers/turnosController');
-const verifyToken = require('../middleware/authMiddleware');
+const { verifyToken } = require('../middleware/authMiddleware');
 
 // Crear nuevo turno (POST)
 router.post('/crear', crearTurno);
 
 // Obtener turnos de un paciente con token (GET)
 router.get('/mis-turnos', verifyToken, obtenerTurnosPorPaciente);
+
+console.log('Tipo de verifyToken:', typeof verifyToken);
+console.log('Tipo de obtenerTurnosPorPaciente:', typeof obtenerTurnosPorPaciente);
 
 module.exports = router;
