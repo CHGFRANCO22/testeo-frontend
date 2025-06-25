4b53b23750dc7ba3d65033c0ce5cc34c8c3e93f9
CHGFRANCO22 <francopudge22@gmail.com>
2025-06-24 22:33:06 -0300

    Update pacientes.js

--- backend/routes/pacientes.js
+++ backend/routes/pacientes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const pacientesController = require('../controllers/pacientesController');
 const { verifyToken, authorizeRoles } = require('../middleware/authMiddleware');
-const verifyToken = require('../middlewares/verifyToken');
 router.put('/:id', verifyToken, pacientesController.updatePaciente);
 // Obtener todos los pacientes (admin y secretaria)
 router.get('/', verifyToken, authorizeRoles('admin', 'secretaria'), pacientesController.getAllPacientes);
